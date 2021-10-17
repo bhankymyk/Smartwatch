4b80171e76850fdfb32860b88c481a71cf7ca681
bhankymyk <bankole123michael@gmail.com>
2021-10-17 17:54:14 +0100

    first commit

---
+++ watch.js
@@ -0,0 +1,20 @@
+function black() {
+    document.getElementById("watch").src = "https://i.imgur.com/iOeUBV7.png"
+}
+function red() {
+    document.getElementById("watch").src = "https://i.imgur.com/iOeUBV7.png"
+}
+function blue() {
+    document.getElementById("watch").src = "https://i.imgur.com/iOeUBV7.png"
+}
+function pink() {
+    document.getElementById("watch").src = "https://i.imgur.com/iOeUBV7.png"
+}
+function purple() {
+    document.getElementById("watch").src = "https://i.imgur.com/iOeUBV7.png"
+}
+function timer(){
+    document.getElementById("heart").style.visibility = 'hidden';
+    document.getElementById("rate").style.visibility = 'hidden';
+    document.getElementById("time").style.visibility = 'visible';
+}
