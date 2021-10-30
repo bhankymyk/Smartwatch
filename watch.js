af0753bee2f62a78cac1be1fd4980e05879dfd6c
bhankymyk <bankole123michael@gmail.com>
2021-10-30 03:21:46 +0100

    watch js updated

--- watch.js
+++ watch.js
@@ -2,16 +2,16 @@ function black() {
     document.getElementById("watch").src = "https://i.imgur.com/iOeUBV7.png"
 }
 function red() {
-    document.getElementById("watch").src = "https://i.imgur.com/iOeUBV7.png"
+    document.getElementById("watch").src = "https://i.imgur.com/PTgQlim.png"
 }
 function blue() {
-    document.getElementById("watch").src = "https://i.imgur.com/iOeUBV7.png"
+    document.getElementById("watch").src = "https://i.imgur.com/Mplj1YR.png"
 }
 function pink() {
-    document.getElementById("watch").src = "https://i.imgur.com/iOeUBV7.png"
+    document.getElementById("watch").src = "https://i.imgur.com/xSIK4M8.png"
 }
 function purple() {
-    document.getElementById("watch").src = "https://i.imgur.com/iOeUBV7.png"
+    document.getElementById("watch").src = "https://i.imgur.com/Zygu7I3.png"
 }
 function timer(){
     document.getElementById("heart").style.visibility = 'hidden';
